1bb479634a139b82b6fc5702620dafffd0888382
Adam Abdulkadir <78934570+aabdulk862@users.noreply.github.com>
2025-03-03 19:44:54 -0500

    Chapter 3 complete

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourPackageRepository.java
@@ -2,9 +2,11 @@ package com.example.explorecalijpa.repo;
 
 import com.example.explorecalijpa.model.TourPackage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage, String> {
     Optional<TourPackage> findByName(String name);
 }
